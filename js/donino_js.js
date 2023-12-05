516013c0bf5a7c81fb1522bbc02968384d5b1f09
Jeon Wan-jin <jwj1212121@gmail.com>
2023-12-06 00:44:43 +0900

    12-06

---
+++ js/donino_js.js
@@ -0,0 +1,6 @@
+const navMoreBtn = document.querySelector('.btn_more');
+const subNav = document.querySelector('.sub_nav');
+
+navMoreBtn.addEventListener('click',function(){
+    subNav.classList.toggle('active');
+});
